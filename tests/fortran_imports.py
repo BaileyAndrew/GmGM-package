d66cf633765f83d0c653bab4c6948669d1fc2eaa
Bailey Andrew <e.bailey.andrew@gmail.com>
2023-11-06 16:54:40 +0000

    Beat head against wall.

--- tests/fortran_imports.py
+++ tests/fortran_imports.py
@@ -1,9 +1,7 @@
 import sys
-sys.path.append('./src/GmGM_BaileyA')
-sys.path.append('./build')
-
-import GmGM
+print("HI======================")
 import fortran_core
+import GmGM
 
 print(fortran_core.__doc__)
 print(fortran_core.fortran_core.extract_d_values.__doc__)
